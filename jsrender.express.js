c87246881121d68996d320a39491ccbfdd1b51c8
Alex Pilon <alex.pilon@gmail.com>
2011-10-03 00:22:07 +0000

    added support for expresses partial method

--- jsrender.express.js
+++ jsrender.express.js
@@ -1,8 +1,22 @@
 var jsrender = require('./jsrender');
 
+jsrender.views.registerTags({
+	"partial": function (path, opts) {
+		return this.data.partial(path, opts || {});
+	},
+	"layout": function () {
+//		return this.data.layout();
+		return "call layout..";
+	}
+});
+
 exports.compile = function (markup, options) {
+	jsrender.views.allowCode = true;
+
 	options = options || {};
 	var name = options.filename || markup;
+
+	delete jsrender.views.templates[name];
 
 	jsrender.template(name, markup);
 
